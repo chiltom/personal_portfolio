90660e4cf11ecf89a5fc5ff07a9c4622841e8199
Tom Childress <thomas.childress02@gmail.com>
2024-03-17 22:28:14 -0400

    functional carousel, add better image components

--- front-end/src/pages/HomePage.tsx
+++ front-end/src/pages/HomePage.tsx
@@ -1,9 +1,35 @@
 import { ReactNode } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import image1 from "../assets/IMG_0439.jpg";
+import image2 from "../assets/IMG_0672.jpg.jpeg";
 
 const HomePage = (): ReactNode => {
   return (
     <>
-      <h1>HomePage</h1>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            src={image1}
+            alt="Picture of me and Bailey"
+            style={{ width: "400px", height: "400px" }}
+          />
+          <Carousel.Caption>
+            <h3>My Wonderful Family</h3>
+            <p>My wife, Bailey</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src={image2}
+            alt="Me"
+            style={{ width: "400px", height: "400px" }}
+          />
+          <Carousel.Caption>
+            <h3>Me</h3>
+            <p>A little bit about me</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </>
   );
 };
