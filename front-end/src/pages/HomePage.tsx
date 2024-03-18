3bce913d3d937fa1e8666ec42cd267d3ecfa05ed
Tom Childress <thomas.childress02@gmail.com>
2024-03-18 15:28:13 -0400

    created carousel for home page important stuff

--- front-end/src/pages/HomePage.tsx
+++ front-end/src/pages/HomePage.tsx
@@ -1,35 +1,10 @@
 import { ReactNode } from "react";
-import Carousel from "react-bootstrap/Carousel";
-import image1 from "../assets/IMG_0439.jpg";
-import image2 from "../assets/IMG_0672.jpg.jpeg";
+import HomeCarousel from "../components/HomeCarousel";
 
 const HomePage = (): ReactNode => {
   return (
     <>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            src={image1}
-            alt="Picture of me and Bailey"
-            style={{ width: "400px", height: "400px" }}
-          />
-          <Carousel.Caption>
-            <h3>My Wonderful Family</h3>
-            <p>My wife, Bailey</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            src={image2}
-            alt="Me"
-            style={{ width: "400px", height: "400px" }}
-          />
-          <Carousel.Caption>
-            <h3>Me</h3>
-            <p>A little bit about me</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <HomeCarousel />
     </>
   );
 };
