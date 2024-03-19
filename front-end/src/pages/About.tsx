d97cf5f6dc6dd80eff6a7d138b680e8bf256b324
Tom Childress <thomas.childress02@gmail.com>
2024-03-18 20:35:39 -0400

    added more content to about page

--- front-end/src/pages/About.tsx
+++ front-end/src/pages/About.tsx
@@ -13,8 +13,8 @@ const About = (): ReactNode => {
         <Row>
           <p>
             Hey! I'm Tom, an Army veteran transitioning back to the real world
-            and working towards my first software engineering role. I started my
-            coding journey when I first went to college at the University of
+            and working towards my first software engineering role. I started on
+            this expedition when I first went to college at the University of
             Connecticut, but found myself quickly running out of funds. I saw
             the military had a great tuition assistance program to help pay for
             school while I was in, as well as that sweet GI Bill at the end of
@@ -25,16 +25,34 @@ const About = (): ReactNode => {
         <br />
         <Row>
           <p>
-            The Army didn't instantly aid my coding journey, and I spent a lot
-            of my free time researching as much as I could about software
+            The Army didn't instantly aid my developer journey, and I spent the
+            little free time I had researching as much as I could about software
             development, as well as slowly working towards my Computer Science
-            degree. Now, transitioning out of the military after 6 years with 90
+            degree. Now, transitioning out of the military with a family, 90
             credits towards my Bachelor's degree completed, and attending an
-            amazing boot camp, Code Platoon, that is rounding all of the edges
-            and honing my development skills better than I could've imagined, I
-            find one detail surprisingly important to my growth that both realms
-            shared: problem-solving.
+            amazing boot camp, Code Platoon, I find myself very appreciative of
+            everything I have gained over the past 6 years.
           </p>
+        </Row>
+        <br />
+        <Row>
+          <p>
+            Problem-solving is the backbone of what I have done and will
+            continue to do every day. Whether it was engineering a way to repair
+            a critical helicopter component, or now studying and implementing
+            complex algorithms and data structures, my work has always involved
+            a great deal of critical thinking. I take great pride and joy in
+            considering myself a life-long learner because of this, and continue
+            to study and implement the highest-quality, most efficient solutions
+            in everything I do.
+          </p>
+        </Row>
+        <br />
+        <Row>
+          <h1 className="text-3xl">Notable Accomplishments</h1>
+          <ul>
+            <li></li>
+          </ul>
         </Row>
       </Container>
     </>
